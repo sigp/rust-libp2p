0c468bd28ba86bb30c4a90901ff95b70ea8ac8c9
pawan <pawandhananjay@gmail.com>
2019-09-17 17:46:49 +0530

    Add functions for issuing and verifying tickets

--- misc/discv5/src/topic.rs
+++ misc/discv5/src/topic.rs
@@ -10,12 +10,14 @@ const MAX_ENTRIES_PER_TOPIC: usize = 50;
 
 pub type TopicHash = [u8; 32];
 
+/// TODO: change to some unique identifier type
+pub type TicketId = String;
+
 /// Representation of a ticket issued to peer for topic registration.
 #[derive(Debug, Clone)]
 pub struct Ticket<TPeerId> {
     /// Unique identifier for ticket.
-    /// TODO: change to some unique identifier type
-    id: String,
+    id: TicketId,
     /// Id of peer to which ticket is issued.
     peer_id: TPeerId,
     /// Wait time for ticket to be allowed for topic registration.
@@ -31,6 +33,15 @@ impl<TPeerId> Ticket<TPeerId> {
             peer_id: peer,
             wait_time: Duration::from_secs(wait_time),
             created_time: Instant::now(),
+        }
+    }
+
+    /// Checks if wait time for ticket has passed.
+    pub fn has_wait_elapsed(&self) -> bool {
+        if self.created_time + self.wait_time < Instant::now() {
+            return false;
+        } else {
+            return true;
         }
     }
 }
@@ -78,6 +89,11 @@ impl<TPeerId> TopicQueue<TPeerId> {
     pub fn remove_from_queue(&mut self) {
         unimplemented!()
     }
+
+    /// Get wait time for queue.
+    pub fn get_wait_time(&self) -> u64 {
+        unimplemented!()
+    }
 }
 
 /// Global queue containing all topic queues and issued tickets
@@ -85,23 +101,31 @@ impl<TPeerId> TopicQueue<TPeerId> {
 #[derive(Debug)]
 pub struct GlobalTopicQueue<TPeerId> {
     topic_map: BTreeMap<Topic, TopicQueue<TPeerId>>,
-    tickets: Vec<Ticket<TPeerId>>,
+    tickets: BTreeMap<TicketId, Ticket<TPeerId>>,
 }
 
-impl<TPeerId> GlobalTopicQueue<TPeerId> {
+impl<TPeerId> GlobalTopicQueue<TPeerId>
+where
+    TPeerId: Clone,
+{
     pub fn new() -> Self {
         GlobalTopicQueue {
             topic_map: BTreeMap::new(),
-            tickets: Vec::new(),
+            tickets: BTreeMap::new(),
         }
     }
 
+    /// Get combined size of all peers across all topic queues.
     fn get_queue_size(&self) -> usize {
         self.topic_map.iter().map(|(_, v)| v.size()).sum()
     }
 
     /// Add a peer to the topic queue.
-    pub fn add_to_queue(&mut self, peer: TPeerId, topic: Topic) {
+    /// Returns None if ticket doesn't exist or wait time hasn't elapsed.
+    pub fn add_to_queue(&mut self, peer: TPeerId, topic: Topic, ticket: &TicketId) -> Option<()> {
+        if !self.is_ticket_valid(ticket) {
+            return None;
+        }
         if self.get_queue_size() == MAX_ENTRIES {
             self.remove_from_queue();
         }
@@ -112,10 +136,30 @@ impl<TPeerId> GlobalTopicQueue<TPeerId> {
             tq.add_to_queue(peer);
             self.topic_map.insert(topic, tq);
         };
+        Some(())
     }
 
     /// Remove element from one of the queues according to some policy
     pub fn remove_from_queue(&mut self) {
         unimplemented!()
+    }
+
+    pub fn issue_ticket(&mut self, peer: TPeerId, topic: Topic) -> TicketId {
+        let wait_time = self
+            .topic_map
+            .get(&topic)
+            .map(|v| v.get_wait_time())
+            .unwrap_or(0);
+        let ticket = Ticket::new(peer, wait_time);
+        self.tickets.insert(ticket.id.clone(), ticket.clone());
+        ticket.id
+    }
+
+    /// Checks if ticket is registered in map and the wait time has elapsed.
+    pub fn is_ticket_valid(&self, ticket_id: &TicketId) -> bool {
+        if let Some(ticket) = self.tickets.get(ticket_id) {
+            ticket.has_wait_elapsed();
+        }
+        false
     }
 }
