3f82e78ac297b0f62ad3c81773d7e2cd2ad5c77c
Age Manning <Age@AgeManning.com>
2020-08-24 22:10:28 +1000

    Wasm support

--- protocols/gossipsub/src/behaviour.rs
+++ protocols/gossipsub/src/behaviour.rs
@@ -1173,8 +1173,7 @@ impl Gossipsub {
                                 // check the flood cutoff
                                 let flood_cutoff = (backoff_time
                                     + self.config.graft_flood_threshold())
-                                .checked_sub(self.config.prune_backoff())
-                                .unwrap_or_else(|| now);
+                                    - self.config.prune_backoff();
                                 if flood_cutoff > now {
                                     //extra penalty
                                     peer_score.add_penalty(peer_id, 1);
